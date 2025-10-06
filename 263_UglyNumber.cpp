8bf6f0d1c97dbee7799ffefaa3fa15650f82047e
harshithkethavath <harshithketh@outlook.com>
2025-10-06 17:40:10 -0400

    solved 263

---
+++ 263_UglyNumber.cpp
@@ -0,0 +1,20 @@
+// First Solution
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        vector<int> factors = {2, 3, 5};
+
+        for (auto factor: factors) {
+            while (n % factor == 0) {
+                n /= factor;
+            }
+        }
+
+        return n == 1;
+    }
+};
