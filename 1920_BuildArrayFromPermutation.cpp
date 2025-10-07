06738c7c9fa97648cda0a01b5f0c3c841db4e62c
harshithkethavath <harshithketh@outlook.com>
2025-10-07 05:50:41 -0400

    Solved 1920

---
+++ 1920_BuildArrayFromPermutation.cpp
@@ -0,0 +1,14 @@
+// First Solution
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector <int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans.push_back(nums[nums[i]]);
+        }
+
+        return ans;
+    }
+};
